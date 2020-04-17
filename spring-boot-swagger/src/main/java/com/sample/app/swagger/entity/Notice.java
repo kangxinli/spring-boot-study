a9239ee2f347dbea9f7a5dcda0a71dc776409788
kangxinli <kangxinli0201@163.com>
2020-04-17 19:11:54 +0800

    fix: swagger

---
+++ spring-boot-swagger/src/main/java/com/sample/app/swagger/entity/Notice.java
@@ -0,0 +1,27 @@
+package com.sample.app.swagger.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@ApiModel("公告")
+public class Notice {
+	/**
+	 * ID
+	 */
+	@ApiModelProperty("id")
+	private Integer id;
+
+	/**
+	 * 公告内容
+	 */
+	@ApiModelProperty("公告内容")
+	private String content;
+}
